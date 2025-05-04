208fdf11e4c3a78617f94c8cedb8e1bec0c71dc3
Emilton Soosaippillai <emiltons@icloud.com>
2025-05-04 10:22:14 +0100

    Features names change

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,5 +1,5 @@
 def get_top_products(df, n = 5):
-    return df.groupby('product')['total_price'].sum().sort_values(ascending = False). head(n)
+    return df.groupby('product_name')['total_price'].sum().sort_values(ascending = False). head(n)
 
 def get_revenue_by_month(df):
-    return df.set_index('date').resample('M')['total_price'].sum()
+    return df.set_index('order_date').resample('M')['total_price'].sum()
--- app/data_loader.py
+++ app/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def load_data(filepath):
-    df = pd.read_csv(filepath, parse_dates = ['date'])
-    df['total_price'] = df['quantity'] * df['price_per_unit']
+    df = pd.read_csv(filepath, parse_dates = ['order_date'])
+    df['total_price'] = df['quantity'] * df['unit_price']
     return df
